077fef98161d326a2870ea18c2017a7ce4d4a18f
Mohamed Hussein <mohamed.hussein@kinexon.com>
2021-12-23 16:31:36 +0000

    Feature/rp 1100 establish backend connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'screens/signin_screen.dart';
+import 'package:provider/provider.dart';
+import 'providers/auth.dart';
 
 void main() {
   runApp(const DemoMobileApp());
@@ -9,37 +12,15 @@ class DemoMobileApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Demo Mobile APP',
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-      ),
-      home: const HomePage(title: 'Kinexon'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello Kinexians!',
-          style: Theme.of(context).textTheme.headline6,
+    return ChangeNotifierProvider(
+      create: (context) => Auth(),
+      child: MaterialApp(
+        title: 'Demo Mobile APP',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.lightBlue,
         ),
+        home: const SigninScreen(),
       ),
     );
   }
---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../widgets/feedback_container_widget.dart';
+import '../providers/auth.dart';
+
+class SigninScreen extends StatefulWidget {
+  const SigninScreen({Key? key}) : super(key: key);
+
+  @override
+  _SigninScreenState createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
+  final usernameController = TextEditingController(text: 'kinexon');
+  final passwordController = TextEditingController(text: 'kinexon');
+  final targetBackendUrlController =
+      TextEditingController(text: 'http://mohamed-hussein.base.knx');
+  bool _isError = false;
+  bool _isLoading = false;
+  static const errorInvalidCredentials = 'Invalid username or password';
+  static const errorInvalidBackendUrl = 'Invalid target backend URL';
+  static const errorInvalidPath = 'Could not connect to the server';
+  String _errorMessage = '';
+
+  final Divider _lineDivider =
+      Divider(color: Colors.grey.shade300, height: 1, thickness: 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade800,
+        title: const Image(image: AssetImage('assets/images/logo.png')),
+        actions: [
+          IconButton(
+            onPressed: () => showDialog(
+              context: context,
+              builder: (context) {
+                _isError = false;
+                return StatefulBuilder(builder: (context, setState) {
+                  return AlertDialog(
+                    title: const Text('Target backend URL'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (_isError)
+                          const FeedbackContainer(
+                              errorMessage: errorInvalidBackendUrl),
+                        TextFormField(
+                          maxLines: 2,
+                          controller: targetBackendUrlController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              fillColor: Color(0xfff3f3f3),
+                              filled: true),
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        child: const Text('Close'),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      TextButton(
+                        child: _isLoading
+                            ? const CircularProgressIndicator()
+                            : const Text('Update link'),
+                        onPressed: () async {
+                          _isError = false;
+                          setState(() => _isLoading = true);
+                          try {
+                            await Provider.of<Auth>(context, listen: false)
+                                .validateTargetBackend(
+                                    targetBackendUrlController.text);
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    '${targetBackendUrlController.text} \nis now being used.'),
+                              ),
+                            );
+
+                            Navigator.of(context).pop();
+                          } catch (error) {
+                            _errorMessage = errorInvalidBackendUrl;
+                            setState(() => _isError = true);
+                          }
+                          setState(() => _isLoading = false);
+                        },
+                      ),
+                    ],
+                  );
+                });
+              },
+            ),
+            icon: const Icon(Icons.link),
+            color: Colors.white70,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Card(
+          elevation: 16,
+          margin: const EdgeInsets.all(16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: _isLoading
+                    ? const CircularProgressIndicator()
+                    : Text(
+                        'Sign in',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w900,
+                            color: Theme.of(context).primaryColorDark),
+                      ),
+              ),
+              if (_isError) FeedbackContainer(errorMessage: _errorMessage),
+              _lineDivider,
+              TextFormField(
+                controller: usernameController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    labelText: 'Username',
+                    suffixIcon: Icon(Icons.person),
+                    border: InputBorder.none,
+                    fillColor: Color(0xfff3f3f3),
+                    filled: true),
+              ),
+              _lineDivider,
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: 'Password',
+                    suffixIcon: Icon(Icons.lock),
+                    border: InputBorder.none,
+                    fillColor: Color(0xfff3f3f3),
+                    filled: true),
+              ),
+              ListTile(
+                onTap: () async => await _onSigninTap(context),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                ),
+                tileColor: Theme.of(context).primaryColorDark,
+                title: const Text(
+                  'Sign in',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _onSigninTap(BuildContext context) async {
+    setState(() => _isLoading = true);
+
+    try {
+      final statusCode = await Provider.of<Auth>(context, listen: false).signIn(
+          targetBackendUrlController.text,
+          usernameController.text,
+          passwordController.text);
+
+      if (statusCode == 200) {
+        _errorMessage = '';
+        //@TODO redirect to next view
+      } else if (statusCode == 401) {
+        _errorMessage = errorInvalidCredentials;
+      } else if (statusCode == 404) {
+        _errorMessage = errorInvalidPath;
+      }
+    } catch (error) {
+      _errorMessage = errorInvalidBackendUrl;
+    }
+
+    setState(() {
+      _isError = _errorMessage != '';
+      _isLoading = false;
+    });
+  }
+}
