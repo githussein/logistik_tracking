fd86ae4e7e4caa6ce8f9c88cab7a419046315aed
Mohamed Hussein <mohamed.hussein@kinexon.com>
2022-05-20 10:46:14 +0200

    OS-211 makes the map url configurable

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import '../widgets/custom_app_bar_widget.dart';
 import '../providers/auth.dart';
+import '../widgets/feedback_container_widget.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -17,6 +17,9 @@ final Completer<WebViewController> _completer = Completer<WebViewController>();
 
 class _MapScreenState extends State<MapScreen> {
   late WebViewController _controller;
+  var targetBackendUrlController = TextEditingController(text: '');
+  bool _isLoading = false;
+  bool _isError = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +28,110 @@ class _MapScreenState extends State<MapScreen> {
       _controller = controller;
 
       _controller.loadUrl(
-          Uri.encodeFull('http://mohamed-hussein.base.knx/riot/embed/map?location=5,3'),
+          Uri.encodeFull('http://mohamed-hussein.base.knx/riot/embed/map'),
           headers: basicAuthHeader);
     });
 
     return Scaffold(
-      appBar: CustomAppBar(screenTitle: AppLocalizations.of(context)!.map),
-      body: SafeArea(
-        child: WebView(
-          debuggingEnabled: true,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (controller) async {
-            _completer.complete(controller);
-          },
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade800,
+        title: Text(
+          AppLocalizations.of(context)!.map,
+          style: const TextStyle(color: Colors.white),
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.link),
+            color: Colors.white70,
+            onPressed: () => showDialog(
+              context: context,
+              builder: (context) {
+                return StatefulBuilder(builder: (context, setState) {
+                  return AlertDialog(
+                    title: Text(AppLocalizations.of(context)!.targetUrl),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (_isError)
+                          FeedbackContainer(
+                              errorMessage: AppLocalizations.of(context)!
+                                  .errorInvalidBackendUrl),
+                        TextFormField(
+                          maxLines: 2,
+                          controller: targetBackendUrlController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              fillColor: Color(0xfff3f3f3),
+                              filled: true),
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        child: Text(AppLocalizations.of(context)!.close),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      TextButton(
+                        child: _isLoading
+                            ? const CircularProgressIndicator()
+                            : Text(AppLocalizations.of(context)!.updateLink),
+                        onPressed: () async {
+                          _isError = false;
+                          setState(() => _isLoading = true);
+
+                          try {
+                            await Provider.of<Auth>(context, listen: false)
+                                .validateTargetBackend(
+                                    targetBackendUrlController.text);
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    '${targetBackendUrlController.text} \n${AppLocalizations.of(context)!.baseUrlUpdated}.'),
+                              ),
+                            );
+
+                            Navigator.pop(context);
+                            _controller.loadUrl(targetBackendUrlController.text,
+                                headers: basicAuthHeader);
+                          } catch (error) {
+                            _errorMessage = AppLocalizations.of(context)!
+                                .errorInvalidBackendUrl;
+                            setState(() => _isError = true);
+                          }
+                          setState(() => _isLoading = false);
+                        },
+                      ),
+                    ],
+                  );
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+      body:
+          //     Center(
+          //       child: Link(
+          //         uri: Uri.parse(
+          //             'https://pub.dev/'),
+          //         target: LinkTarget.self,
+          //         builder: (BuildContext ctx, FollowLink? openLink) {
+          //           return TextButton.icon(
+          //             onPressed: openLink,
+          //             label: const Text('Link Widget documentation'),
+          //             icon: const Icon(Icons.read_more),
+          //           );
+          //         },
+          //       ),
+          // ),
+          WebView(
+        debuggingEnabled: true,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (controller) async {
+          _completer.complete(controller);
+        },
       ),
     );
   }
